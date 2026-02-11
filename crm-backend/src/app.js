2b37a4725820676e9887349e95f3dcdec7f8a6bb
Mary Lavanitha <lavanitha.officialdesk@gmail.com>
2026-02-11 15:14:36 +0530

    Update app.js

--- crm-backend/src/app.js
+++ crm-backend/src/app.js
@@ -2,14 +2,21 @@ import express from "express";
 import cors from "cors";
 import dashboardRoutes from "./routes/dashboardRoutes.js";
 
-
 const app = express();
 
-// Middlewares
-app.use(cors());
+// Proper CORS config
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://customer-relationship.netlify.app"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true
+}));
+
 app.use(express.json());
 app.use("/api/dashboard", dashboardRoutes);
-
 
 // Health check
 app.get("/", (req, res) => {
